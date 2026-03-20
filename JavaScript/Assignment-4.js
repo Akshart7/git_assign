676c73c72e21f5bc026d83ec148f61bc30fb66e2
Akshart7 <167081541+Akshart7@users.noreply.github.com>
2026-03-20 10:28:23 +0530

    Add files via upload

---
+++ JavaScript/Assignment-4.js
@@ -0,0 +1,13 @@
+let isMember = true
+
+let cartValue;
+
+cartValue = 1000
+
+if (isMember && cartValue > 1000) {
+    console.log('You are eligible for a free delivery!')
+} else if (cartValue > 2000) {
+    console.log('You are eligible for a free delivery!')
+} else {
+    console.log('You are not eligible for a free delivery.')
+}
